75d0e01609f9a564ca22c7bb334489c9e729d98b
Muhamad Fatah <muhamad.fatah494@gmail.com>
2022-05-31 12:35:15 +0700

    tugas 3 react js
    tugas 3 react js - niomic

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Footer from "./Page/Footer";
+import MenuMakanan from "./Page/MenuMakanan";
+import MenuTentangKami from "./Page/MenuTentangKami";
+import MenuKontak from "./Page/MenuKontak";
+import Header from "./Page/Header";
+
+function App() {
+  return (
+    <div>
+      <Header/>
+
+      <MenuMakanan/>
+
+      <MenuTentangKami />
+      <MenuKontak/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Page/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    <div>
+        <center>
+            <h3>Home | Product | Kontak | Tentang Kami </h3>
+        </center>
+    </div>
+}
+
+export default Header;
